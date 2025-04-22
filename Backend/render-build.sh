12918b9f314a2da18d115e1e47027f85609a793d
Mehul001 <mehulverma17@gmail.com>
2025-04-22 23:46:22 +0530

    for render_3

--- Backend/render-build.sh
+++ Backend/render-build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
+# exit on errors
+set -o errexit
 
+# Install the necessary dependencies
 npm install
-# npm run build # uncomment if required
 
-# Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache" 
+# Ensure Puppeteer cache is set up correctly
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Puppeteer Cache directory not found, creating new cache."
+  mkdir -p $PUPPETEER_CACHE_DIR
+fi
+
+# Copy Puppeteer cache to the build directory if not already present
+if [[ ! -d /opt/render/.cache/puppeteer/ ]]; then
+  echo "...Copying Puppeteer Cache from Build Cache"
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
+else
+  echo "...Storing Puppeteer Cache in Build Cache"
   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
