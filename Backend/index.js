d7005db846019f3dbc38c4fb09d2580cacdea5a5
Mehul001 <mehulverma17@gmail.com>
2025-04-23 23:06:59 +0530

    for render

--- Backend/index.js
+++ Backend/index.js
@@ -50,6 +50,10 @@ app.post("/generate-pdf", async (req, res) => {
         "--single-process",
         "--no-zygote",
       ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
